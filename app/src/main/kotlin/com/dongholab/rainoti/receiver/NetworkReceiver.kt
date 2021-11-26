ce8bcc2b1f4c83736664db7528f6ceda70dd63ce
Dongho Gang <gongdongho12@gmail.com>
2021-11-26 19:03:34 +0900

    feat: 네트워크 변경사항 반영 리시버 추가

---
+++ app/src/main/kotlin/com/dongholab/rainoti/receiver/NetworkReceiver.kt
@@ -0,0 +1,18 @@
+package com.dongholab.rainoti.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.util.Log
+
+class NetworkReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val conn = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo? = conn.activeNetworkInfo
+
+        Log.d("networkChange", networkInfo.toString())
+    }
+}
