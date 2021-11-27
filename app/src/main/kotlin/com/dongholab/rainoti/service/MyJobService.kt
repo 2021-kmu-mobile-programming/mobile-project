b1f06a75ea23e51ebab5dcd01466397a8a17618b
Dongho Gang <gongdongho12@gmail.com>
2021-11-28 05:04:01 +0900

    feat: 네트워크 리시버 테스트

--- app/src/main/kotlin/com/dongholab/rainoti/service/MyJobService.kt
+++ app/src/main/kotlin/com/dongholab/rainoti/service/MyJobService.kt
@@ -1,17 +1,22 @@
-package com.dongholab.rainoti.receiver
+package com.dongholab.rainoti.service
 
-import android.content.BroadcastReceiver
+import android.app.job.JobParameters
+import android.app.job.JobService
 import android.content.Context
-import android.content.Intent
 import android.net.*
-import android.net.ConnectivityManager.NetworkCallback
 import android.util.Log
 
-class NetworkReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
-        val conn = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+class MyJobService : JobService() {
+    companion object {
+        private val TAG = "MyJobService"
+    }
+
+    override
+    fun onStartJob(params: JobParameters): Boolean {
+        Log.d(TAG, "onStartJob: ${params.jobId}")
+        val conn = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val builder: NetworkRequest.Builder = NetworkRequest.Builder()
-        conn.registerNetworkCallback(builder.build(), object : NetworkCallback() {
+        val callback: ConnectivityManager.NetworkCallback = object: ConnectivityManager.NetworkCallback() {
             override fun onAvailable(network: Network) {
                 // 네트워크를 사용할 준비가 되었을 때
                 Log.d("onAvailable", network.toString())
@@ -29,7 +34,21 @@ class NetworkReceiver : BroadcastReceiver() {
             override fun onLinkPropertiesChanged(network : Network, linkProperties : LinkProperties) {
                 Log.e("onLinkPropertiesChanged", "The default network changed link properties: " + linkProperties)
             }
-        })
+        }
+        conn.registerNetworkCallback(builder.build(), callback)
 
+        Thread {
+            Thread.sleep(1000)
+            Log.d(TAG, "doing Job in other thread")
+            conn.unregisterNetworkCallback(callback)
+            jobFinished(params, false)
+        }
+        return false
+    }
+
+    override
+    fun onStopJob(params: JobParameters): Boolean {
+        Log.d(TAG, "onStopJob: ${params.jobId}")
+        return false
     }
 }
