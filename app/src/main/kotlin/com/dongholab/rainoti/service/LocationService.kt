1cea604354f1aea08f76100f37bce16b391abdef
Dongho Gang <gongdongho12@gmail.com>
2021-11-28 05:08:00 +0900

    feat: 위치 서비스에 네트워크 정보 업데이트 될 수 있도록 수정

--- app/src/main/kotlin/com/dongholab/rainoti/service/LocationService.kt
+++ app/src/main/kotlin/com/dongholab/rainoti/service/LocationService.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.location.Location
+import android.net.*
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
@@ -57,6 +58,7 @@ class LocationService : Service() {
     private val localBinder = LocalBinder()
 
     private lateinit var notificationManager: NotificationManager
+    private lateinit var connecitvityManager: ConnectivityManager
 
     // TODO: Step 1.1, Review variables (no changes).
     // FusedLocationProviderClient - Main class for receiving location updates.
@@ -69,6 +71,8 @@ class LocationService : Service() {
     // LocationCallback - Called when FusedLocationProviderClient has a new Location.
     private lateinit var locationCallback: LocationCallback
 
+    private lateinit var connectvityCallback: ConnectivityManager.NetworkCallback
+
     // Used only for local storage of the last known location. Usually, this would be saved to your
     // database, but because this is a simplified sample without a full database, we only need the
     // last location to create a Notification if the user navigates away from the app.
@@ -78,54 +82,28 @@ class LocationService : Service() {
         Log.d(TAG, "onCreate()")
 
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        connecitvityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val builder: NetworkRequest.Builder = NetworkRequest.Builder()
 
-        // TODO: Step 1.2, Review the FusedLocationProviderClient.
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        // TODO: Step 1.3, Create a LocationRequest.
         locationRequest = LocationRequest.create().apply {
-            // Sets the desired interval for active location updates. This interval is inexact. You
-            // may not receive updates at all if no location sources are available, or you may
-            // receive them less frequently than requested. You may also receive updates more
-            // frequently than requested if other applications are requesting location at a more
-            // frequent interval.
-            //
-            // IMPORTANT NOTE: Apps running on Android 8.0 and higher devices (regardless of
-            // targetSdkVersion) may receive updates less frequently than this interval when the app
-            // is no longer in the foreground.
             interval = TimeUnit.SECONDS.toMillis(60)
-
-            // Sets the fastest rate for active location updates. This interval is exact, and your
-            // application will never receive updates more frequently than this value.
             fastestInterval = TimeUnit.SECONDS.toMillis(30)
-
-            // Sets the maximum time when batched location updates are delivered. Updates may be
-            // delivered sooner than this interval.
             maxWaitTime = TimeUnit.MINUTES.toMillis(2)
-
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
 
-        // TODO: Step 1.4, Initialize the LocationCallback.
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
                 super.onLocationResult(locationResult)
 
-                // Normally, you want to save a new location to a database. We are simplifying
-                // things a bit and just saving it as a local variable, as we only need it again
-                // if a Notification is created (when the user navigates away from app).
                 currentLocation = locationResult.lastLocation
 
-                // Notify our Activity that a new location was added. Again, if this was a
-                // production app, the Activity would be listening for changes to a database
-                // with new locations, but we are simplifying things a bit to focus on just
-                // learning the location side of things.
                 val intent = Intent(ACTION_FOREGROUND_ONLY_LOCATION_BROADCAST)
                 intent.putExtra(EXTRA_LOCATION, currentLocation)
                 LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent)
 
-                // Updates notification content if this service is running as a foreground
-                // service.
                 if (serviceRunningInForeground) {
                     notificationManager.notify(
                         NOTIFICATION_ID,
@@ -133,27 +111,45 @@ class LocationService : Service() {
                 }
             }
         }
+
+        connectvityCallback = object: ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                // 네트워크를 사용할 준비가 되었을 때
+                Log.d("onAvailable", network.toString())
+            }
+
+            override fun onLost(network: Network) {
+                // 네트워크가 끊겼을 때
+                Log.d("onLost", network.toString())
+            }
+
+            override fun onCapabilitiesChanged(network : Network, networkCapabilities : NetworkCapabilities) {
+                Log.e("onCapabilitiesChanged", "The default network changed capabilities: " + networkCapabilities)
+            }
+
+            override fun onLinkPropertiesChanged(network : Network, linkProperties : LinkProperties) {
+                Log.e("onLinkPropertiesChanged", "The default network changed link properties: " + linkProperties)
+            }
+        }
+        connecitvityManager.registerNetworkCallback(builder.build(), connectvityCallback)
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         Log.d(TAG, "onStartCommand()")
 
-        val cancelLocationTrackingFromNotification =
-            intent.getBooleanExtra(EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION, false)
+        val cancelLocationTrackingFromNotification = intent.getBooleanExtra(EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION, false)
 
         if (cancelLocationTrackingFromNotification) {
             unsubscribeToLocationUpdates()
             stopSelf()
         }
-        // Tells the system not to recreate the service after it's been killed.
+
         return START_NOT_STICKY
     }
 
     override fun onBind(intent: Intent): IBinder {
         Log.d(TAG, "onBind()")
 
-        // MainActivity (client) comes into foreground and binds to service, so the service can
-        // become a background services.
         stopForeground(true)
         serviceRunningInForeground = false
         configurationChange = false
@@ -163,21 +159,16 @@ class LocationService : Service() {
     override fun onRebind(intent: Intent) {
         Log.d(TAG, "onRebind()")
 
-        // MainActivity (client) returns to the foreground and rebinds to service, so the service
-        // can become a background services.
         stopForeground(true)
         serviceRunningInForeground = false
         configurationChange = false
+        connecitvityManager.unregisterNetworkCallback(connectvityCallback)
         super.onRebind(intent)
     }
 
     override fun onUnbind(intent: Intent): Boolean {
         Log.d(TAG, "onUnbind()")
 
-        // MainActivity (client) leaves foreground, so service needs to become a foreground service
-        // to maintain the 'while-in-use' label.
-        // NOTE: If this method is called due to a configuration change in MainActivity,
-        // we do nothing.
         if (!configurationChange && SharedPreferenceUtil.getLocationTrackingPref(this)) {
             Log.d(TAG, "Start foreground service")
             val notification = generateNotification(currentLocation)
@@ -191,6 +182,7 @@ class LocationService : Service() {
 
     override fun onDestroy() {
         Log.d(TAG, "onDestroy()")
+        connecitvityManager.unregisterNetworkCallback(connectvityCallback)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
@@ -202,14 +194,9 @@ class LocationService : Service() {
         Log.d(TAG, "subscribeToLocationUpdates()")
 
         SharedPreferenceUtil.saveLocationTrackingPref(this, true)
-
-        // Binding to this service doesn't actually trigger onStartCommand(). That is needed to
-        // ensure this Service can be promoted to a foreground service, i.e., the service needs to
-        // be officially started (which we do here).
         startService(Intent(applicationContext, LocationService::class.java))
 
         try {
-            // TODO: Step 1.5, Subscribe to location changes.
             fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
         } catch (unlikely: SecurityException) {
             SharedPreferenceUtil.saveLocationTrackingPref(this, false)
@@ -221,7 +208,6 @@ class LocationService : Service() {
         Log.d(TAG, "unsubscribeToLocationUpdates()")
 
         try {
-            // TODO: Step 1.6, Unsubscribe to location changes.
             val removeTask = fusedLocationProviderClient.removeLocationUpdates(locationCallback)
             removeTask.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -239,56 +225,31 @@ class LocationService : Service() {
         }
     }
 
-    /*
-     * Generates a BIG_TEXT_STYLE Notification that represent latest location.
-     */
     private fun generateNotification(location: Location?): Notification {
         Log.d(TAG, "generateNotification()")
 
-        // Main steps for building a BIG_TEXT_STYLE notification:
-        //      0. Get data
-        //      1. Create Notification Channel for O+
-        //      2. Build the BIG_TEXT_STYLE
-        //      3. Set up Intent / Pending Intent for notification
-        //      4. Build and issue the notification
-
-        // 0. Get data
         val mainNotificationText = location?.toText() ?: getString(R.string.no_location_text)
         val titleText = getString(R.string.app_name)
 
-        // 1. Create Notification Channel for O+ and beyond devices (26+).
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-            val notificationChannel = NotificationChannel(
-                NOTIFICATION_CHANNEL_ID, titleText, NotificationManager.IMPORTANCE_DEFAULT)
-
-            // Adds NotificationChannel to system. Attempting to create an
-            // existing notification channel with its original values performs
-            // no operation, so it's safe to perform the below sequence.
+            val notificationChannel = NotificationChannel(NOTIFICATION_CHANNEL_ID, titleText, NotificationManager.IMPORTANCE_DEFAULT)
             notificationManager.createNotificationChannel(notificationChannel)
         }
 
-        // 2. Build the BIG_TEXT_STYLE.
         val bigTextStyle = NotificationCompat.BigTextStyle()
             .bigText(mainNotificationText)
             .setBigContentTitle(titleText)
 
-        // 3. Set up main Intent/Pending Intents for notification.
         val launchActivityIntent = Intent(this, MainActivity::class.java)
 
         val cancelIntent = Intent(this, LocationService::class.java)
         cancelIntent.putExtra(EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION, true)
 
-        val servicePendingIntent = PendingIntent.getService(
-            this, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val servicePendingIntent = PendingIntent.getService(this, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        val activityPendingIntent = PendingIntent.getActivity(
-            this, 0, launchActivityIntent, 0)
+        val activityPendingIntent = PendingIntent.getActivity(this, 0, launchActivityIntent, 0)
 
-        // 4. Build and issue the notification.
-        // Notification Channel Id is ignored for Android pre O (26).
-        val notificationCompatBuilder =
-            NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+        val notificationCompatBuilder = NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
 
         return notificationCompatBuilder
             .setStyle(bigTextStyle)
@@ -298,16 +259,8 @@ class LocationService : Service() {
             .setDefaults(NotificationCompat.DEFAULT_ALL)
             .setOngoing(true)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-            .addAction(
-                R.drawable.ic_home_black_24dp,
-                getString(R.string.launch_activity),
-                activityPendingIntent
-            )
-            .addAction(
-                R.drawable.ic_action_cancel,
-                getString(R.string.stop_location_updates_button_text),
-                servicePendingIntent
-            )
+            .addAction(R.drawable.ic_home_black_24dp,  getString(R.string.launch_activity), activityPendingIntent)
+            .addAction(R.drawable.ic_action_cancel, getString(R.string.stop_location_updates_button_text), servicePendingIntent)
             .build()
     }
 
