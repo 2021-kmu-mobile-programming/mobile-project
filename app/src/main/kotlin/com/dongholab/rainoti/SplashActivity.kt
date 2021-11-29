991e001e9e3b525198e7ccf502d90a005ed944be
Joonhyung Cho <jaycho46@illunex.com>
2021-11-29 12:45:48 +0900

    Add splash activity

---
+++ app/src/main/kotlin/com/dongholab/rainoti/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.dongholab.rainoti
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_splash)
+        logo.alpha = 0f
+        logo.animate().setDuration(1500).alpha(1f).withEndAction{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
